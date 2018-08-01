d4cf16a7db85c7c81e2d55a1d53801ddd2a28bfd
pedro0930 <pedro0930@gmail.com>
2018-08-01 11:45:19 -0700

    selection

--- Program.cs
+++ Program.cs
@@ -14,23 +14,31 @@ namespace cards
             Deck myDeck = new Deck();
             Player Player1 = new Player(Player1Name);
             myDeck.Shuffle(myDeck.DeckOfCards);
-            
+            for (int i = 0; i<=13; i++){
+                Player1.Draw(myDeck.DeckOfCards);
+                }
             // Game starts below this line
-            System.Console.WriteLine("Please input exact command for your action, you can 'draw' or 'discard' if you have card in hand.");
-            string PlayerCommand = Console.ReadLine();
-            while (PlayerCommand != "Quit"){
+            string PlayerCommand = "";
+            while(PlayerCommand != "quit"){
+                System.Console.WriteLine("Please input exact command for your action, you can 'draw' or 'discard' if you have card in hand.");
+                PlayerCommand = Console.ReadLine();
                 if (PlayerCommand == "draw"){
                     Player1.Draw(myDeck.DeckOfCards);
                     }
                 if (PlayerCommand == "discard"){
                     string discardSelection = Console.ReadLine();
-
                     System.Console.WriteLine("Please input exact command for your action, you can 'draw' or 'discard' if you have card in hand.");
                     PlayerCommand = Console.ReadLine();
+                    }
+                if (PlayerCommand == "look"){
+                    Player1.LookAtHand(Player1.PlayerHand);
                 }
- 
+                System.Console.WriteLine("Shit happened");
+                System.Console.WriteLine("Please input exact command for your action, you can 'draw' or 'discard' if you have card in hand.");
+                PlayerCommand = Console.ReadLine();
+                }
             }
         }
         
     }
-}
+
