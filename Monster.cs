e39896db5326169b298ce4a19abdb2d42d24522c
pedro0930 <pedro0930@gmail.com>
2018-08-01 13:17:26 -0700

    Look at card and selection complete

---
+++ Monster.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+
+namespace cards{
+    public class Monster{
+        public int health;
+        public string name;
+
+        public Monster(){
+        this.health = 100;
+        this.name = "Werewolf"
+        }
+    }
+}
