82e679cdf2feb22c44e8a56b6b692c58a5df9fbf
pedro0930 <pedro0930@gmail.com>
2018-08-01 14:15:05 -0700

    merge

--- Monster.cs
+++ Monster.cs
@@ -9,8 +9,12 @@ namespace cards{
         this.health = 100;
         this.name = "The Tester Monster";
         }
-        public void attack(Player target){
+        public void Attack(Player target){
             target.health -=10;
-        }
+                }
+        public void PlayerAttack(int damage){
+                this.health -= damage;
+                System.Console.WriteLine($"{this.name} is attacked for {damage}");
+                }
     }
 }
